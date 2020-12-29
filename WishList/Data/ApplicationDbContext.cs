d02e5c236c7682da24fafd6f069bb16c7ef71107
YKembal <47929576+YKembal@users.noreply.github.com>
2020-12-29 12:05:00 +0200

    3rd.2

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace WishList.Data
     {
 
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
         }
